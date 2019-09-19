efbec9a9405381ded79e2eeca0f77ecf9dd4be8e
Douglas Muth <dmuth@dmuth.org>
2019-09-19 19:34:23 -0400

    Now writing diagnostic messages to stderr and added support for "-" to write to stdout.

--- save.sh
+++ save.sh
@@ -10,23 +10,23 @@
 # Errors are fatal
 set -e
 
-echo "# "
-echo "# Save your development/ directory."
-echo "# "
-echo "# Git Repo: https://github.com/dmuth/save-and-restore-development-directory "
-echo "# "
+>&2 echo "# "
+>&2 echo "# Save your development/ directory."
+>&2 echo "# "
+>&2 echo "# Git Repo: https://github.com/dmuth/save-and-restore-development-directory "
+>&2 echo "# "
 
 if test ! "$2"
 then
-	echo "! "
-	echo "! Syntax: $0 directory output_file"
-	echo "! "
-	echo "! directory - Top-level directory to get Git remotes from. "
-	echo "! 	There can be as much nesting as you like. :-)"
-	echo "! "
-	echo "! output_file - Where to write the directory and Git remote information"
-	echo "! "
-	echo "! "
+	>&2 echo "! "
+	>&2 echo "! Syntax: $0 directory output_file"
+	>&2 echo "! "
+	>&2 echo "! directory - Top-level directory to get Git remotes from. "
+	>&2 echo "! 	There can be as much nesting as you like. :-)"
+	>&2 echo "! "
+	>&2 echo "! output_file - Where to write the directory and Git remote information (Specify a dash for stdout)"
+	>&2 echo "! "
+	>&2 echo "! "
 	exit 1
 fi
 
@@ -34,21 +34,34 @@ SRC=$1
 DEST=$PWD/$2
 ORIG=$PWD
 
+#
+# Save stdout
+#
+exec 6>&1
+
+#
+# If we're not writing to stdout, redirect stdout to our output file.
+#
+if test "$2" != "-"
+then
+	exec 1> $DEST
+fi
+
 pushd $SRC > /dev/null
 
-echo "# "
-echo "# Starting in $SRC..."
-echo "# "
+>&2 echo "# "
+>&2 echo "# Starting in $SRC..."
+>&2 echo "# "
 
 #
 # Remove the destination file if it already exists :-)
 #
-echo -n > $DEST
+echo -n
 
 for DIR in $(find . -name .git -type d)
 do
 
-	echo "Checking ${DIR}..."
+	>&2 echo "Checking ${DIR}..."
 
 	#
 	# Get the name of the directory over the .git directory
@@ -70,13 +83,15 @@ do
 		continue
 	fi
 
-	echo -e "${DIR}\t${REMOTE}" >> $DEST
+	echo -e "${DIR}\t${REMOTE}" 
 
 done
 
-echo "# "
-echo "# Done!  Git repo data written to ${DEST}"
-echo "# "
+exec 1>&6
+
+>&2 echo "# "
+>&2 echo "# Done!  Git repo data written to ${DEST}"
+>&2 echo "# "
 
 
 
